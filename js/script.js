452e0605d7b9af4dcad144c048426434df3935fb
boudidah ilyes <ilyes.boudidah@esprit.tn>
2023-04-14 17:11:57 +0100

    .

--- js/script.js
+++ js/script.js
@@ -68,132 +68,346 @@ const selected = document.querySelector(".selected");
 const optionsContainer = document.querySelector(".option-container");
 const optionsList = document.querySelectorAll(".option");
 
-const box_container=document.querySelector(".box-container");
-
-function product_list(table_name,table_price,table_img)
-  {
-    for(var j=0;j<table_name.length;j++)
-    {
-let div_box=document.createElement("div");
-div_box.classList.add("box");
-
-// div with class image
-let div_img=document.createElement("div");
-div_img.classList.add("image");
-let img = document.createElement("img");
-img.src = "../images/"+table_img[j];
-div_img.appendChild(img);
-// end
-
-// balise div avec class="content"
-let div_content=document.createElement("div");
-div_content.classList.add("content");
-// end
-
-// h3
-let h3=document.createElement("h3");
-h3.textContent=table_name[j];
-// end
-
-// balise div avec class="stars"
-let div_stars=document.createElement("div");
-div_stars.classList.add("stars");
-for(var i=0;i<5;i++)
-{
-let star_i=document.createElement("i");
-star_i.classList.add("fas","fa-star");
-div_stars.appendChild(star_i);
-}
-// end
-
-
-//balise span avec class="price"
-let span_price=document.createElement("span");
-span_price.classList.add("price");
-span_price.textContent=table_price[j]+" DT";
-// end
-
-// form
-let form=document.createElement("form");
-let input=document.createElement("input");
-input.type="number";
-input.value=0;
-form.appendChild(input);
-// end
-
-// balise a avec class="btn"
-let a=document.createElement("a");
-a.classList.add("btn");
-a.textContent="ajouter au panier";
-// end a
-
-div_box.appendChild(div_img);
-div_content.appendChild(h3);
-div_content.appendChild(div_stars);
-div_content.appendChild(span_price);
-div_content.appendChild(form);
-div_content.appendChild(a);
-div_box.appendChild(div_content);
-box_container.appendChild(div_box);
-}
-  }
-  // cakes
+// cakes
 const cakes_name=["Marbré","Blondies","Brownies","Cannelés","Cupcake citron myrtille","cupcakes au chocolat"];
-const cakes_price= [7,5,3,6,9,8];
-const cakes_img=["cake-marbré.jpeg","cake-blondies.jpeg","cake-brownies.jpeg","cake-cannelés.jpeg","cake-cupcake citron myrtille.jpeg","cake-cupcakes au chocolat.jpeg"];
-
 // gateaux
 const gateaux_name=["Entremets chocolat caramel","Entremets noisettes","Fromboisier","Millefeuille Opéra","Opéra"];
-const gateaux_price=[40,35,45,10,5];
-const gateaux_img=["gateau-entremets chocolat caramel.jpeg","gateau-entremets noisettes.jpeg","gateau-framboisier.jpeg","gateau-Millefeuille Opéra.jpg","gateau-opéra.jpg"];
-
 // viennoiseries
 const viennoiseries_name=["Croissants","Croissants avec confiture","viennoiserie à la figue"];
-const viennoiseries_price=[5,7,7];
-const viennoiseries_img=["vin-croissant.jpeg","vin-croissants.jpeg","vin-viennoiserie à la figue.jpeg"];
-
 //tartes
 const tartes_name=["cookies","éclair passion framboise","paris brest"];
-const tartes_price= [3,4,50];
-const tartes_img=["tarte-cookies.jpeg","tarte-éclair passion framboise.jpg","tarte-paris brest.jpeg"];
-var index=0;
-product_list(cakes_name,cakes_price,cakes_img);
+
 selected.addEventListener("click", () => {
     optionsContainer.classList.toggle("active");
   });
+          for(var i=0;i<cakes_name.length;i++)
+        {    let cake=document.getElementById("cake"+(i+1));
+             cake.style.display="";
+        }
+        for(var i=0;i<gateaux_name.length;i++)
+        {   let gateau=document.getElementById("gateau"+(i+1));
+            gateau.style.display='none';
+        }
+        for(var i=0;i<viennoiseries_name.length;i++)
+        {
+             let vin=document.getElementById("vin"+(i+1));
+             vin.style.display='none';
+        }
+        for(var i=0;i<tartes_name.length;i++)
+        {
+            let tarte=document.getElementById("tarte"+(i+1));
+            tarte.style.display='none';
+        }
 optionsList.forEach(o => {
   o.addEventListener("click", () => {
     selected.innerHTML = o.querySelector("label").textContent;
     optionsContainer.classList.remove("active");
-
-while (box_container.lastElementChild) {
-    box_container.removeChild(box_container.lastElementChild);
-}
-if (selected.textContent=="cakes")
-{   index=0;
-    product_list(cakes_name,cakes_price,cakes_img);
-}
-else if (selected.textContent=="gateaux")
-{   index=1;
-    product_list(gateaux_name,gateaux_price,gateaux_img);
-}
-else if(selected.textContent=="viennoiseries")
-{   index=2;
-    product_list(viennoiseries_name,viennoiseries_price,viennoiseries_img);
-}
-else if(selected.textContent=="tartes")
-{   index=3;
-    product_list(tartes_name,tartes_price,tartes_img);
-}
-else
-{   index=4;
-    product_list(pain_name,pain_price,pain_img);
-}
+    switch(selected.textContent)
+    {
+        case "cakes":
+            {
+                for(var i=0;i<cakes_name.length;i++)
+                {    let cake=document.getElementById("cake"+(i+1));
+                     cake.style.display="";
+                }
+                for(var i=0;i<gateaux_name.length;i++)
+                {   let gateau=document.getElementById("gateau"+(i+1));
+                    gateau.style.display='none';
+                }
+                for(var i=0;i<viennoiseries_name.length;i++)
+                {
+                     let vin=document.getElementById("vin"+(i+1));
+                     vin.style.display='none';
+                }
+                for(var i=0;i<tartes_name.length;i++)
+                {
+                    let tarte=document.getElementById("tarte"+(i+1));
+                    tarte.style.display='none';
+                }
+                break;
+            }
+        case "gateaux":
+            {
+                for(var i=0;i<cakes_name.length;i++)
+                {    let cake=document.getElementById("cake"+(i+1));
+                     cake.style.display="none";
+                }
+                for(var i=0;i<gateaux_name.length;i++)
+                {   let gateau=document.getElementById("gateau"+(i+1));
+                    gateau.style.display='';
+                }
+                for(var i=0;i<viennoiseries_name.length;i++)
+                {
+                     let vin=document.getElementById("vin"+(i+1));
+                     vin.style.display='none';
+                }
+                for(var i=0;i<tartes_name.length;i++)
+                {
+                    let tarte=document.getElementById("tarte"+(i+1));
+                    tarte.style.display='none';
+                }
+                break;
+            }
+            case "viennoiseries":
+                {
+                    for(var i=0;i<cakes_name.length;i++)
+                    {    let cake=document.getElementById("cake"+(i+1));
+                         cake.style.display="none";
+                    }
+                    for(var i=0;i<gateaux_name.length;i++)
+                    {   let gateau=document.getElementById("gateau"+(i+1));
+                        gateau.style.display='none';
+                    }
+                    for(var i=0;i<viennoiseries_name.length;i++)
+                    {
+                         let vin=document.getElementById("vin"+(i+1));
+                         vin.style.display='';
+                    }
+                    for(var i=0;i<tartes_name.length;i++)
+                    {
+                        let tarte=document.getElementById("tarte"+(i+1));
+                        tarte.style.display='none';
+                    }
+                    break;
+                }
+                case "tartes":
+                {for(var i=0;i<cakes_name.length;i++)
+                    {    let cake=document.getElementById("cake"+(i+1));
+                         cake.style.display="none";
+                    }
+                    for(var i=0;i<gateaux_name.length;i++)
+                    {   let gateau=document.getElementById("gateau"+(i+1));
+                        gateau.style.display='none';
+                    }
+                    for(var i=0;i<viennoiseries_name.length;i++)
+                    {
+                         let vin=document.getElementById("vin"+(i+1));
+                         vin.style.display='none';
+                    }
+                    for(var i=0;i<tartes_name.length;i++)
+                    {
+                        let tarte=document.getElementById("tarte"+(i+1));
+                        tarte.style.display='';
+                    }
+                    break;
+                }    
+    }
   });
 });
 
 // END CATEGORIES
 
+// PANIER
+for(var i=0;i<cakes_name.length;i++)
+document.getElementById("cake"+(i+1)+"_cart").style.display="none";
+for(var i=0;i<gateaux_name.length;i++)
+document.getElementById("gateau"+(i+1)+"_cart").style.display="none";
+for(var i=0;i<viennoiseries_name.length;i++)
+document.getElementById("vin"+(i+1)+"_cart").style.display="none";
+for(var i=0;i<tartes_name.length;i++)
+document.getElementById("tarte"+(i+1)+"_cart").style.display="none";
+document.getElementById("finaliserbtn").style.display="none";
+let cake_1=document.getElementById("cake_1").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("cake1_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("cake1_price").innerHTML+=document.getElementById("cake1_quantite").value;
+});
+
+let cake_2=document.getElementById("cake_2").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("cake2_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("cake2_price").innerHTML+=document.getElementById("cake2_quantite").value;
+});
+let cake_3=document.getElementById("cake_3").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("cake3_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("cake3_price").innerHTML+=document.getElementById("cake3_quantite").value;
+});
+let cake_4=document.getElementById("cake_4").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("cake4_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("cake4_price").innerHTML+=document.getElementById("cake4_quantite").value;
+});
+let cake_5=document.getElementById("cake_5").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("cake5_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("cake5_price").innerHTML+=document.getElementById("cake5_quantite").value;
+});
+let cake_6=document.getElementById("cake_6").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("cake6_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("cake6_price").innerHTML+=document.getElementById("cake6_quantite").value;
+});
+let gateau_1=document.getElementById("gateau_1").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("gateau1_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("gateau1_price").innerHTML+=document.getElementById("gateau1_quantite").value;
+});
+let gateau_2=document.getElementById("gateau_2").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("gateau2_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("gateau2_price").innerHTML+=document.getElementById("gateau2_quantite").value;
+});
+let gateau_3=document.getElementById("gateau_3").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("gateau3_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("gateau3_price").innerHTML+=document.getElementById("gateau3_quantite").value;
+});
+let gateau_4=document.getElementById("gateau_4").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("gateau4_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("gateau4_price").innerHTML+=document.getElementById("gateau4_quantite").value;
+});
+let gateau_5=document.getElementById("gateau_5").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("gateau5_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("gateau5_price").innerHTML+=document.getElementById("gateau5_quantite").value;
+});
+let vin_1=document.getElementById("vin_1").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("vin1_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("vin1_price").innerHTML+=document.getElementById("vin1_quantite").value;
+});
+let vin_2=document.getElementById("vin_2").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("vin2_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("vin2_price").innerHTML+=document.getElementById("vin2_quantite").value;
+});
+let vin_3=document.getElementById("vin_3").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("vin3_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("vin3_price").innerHTML+=document.getElementById("vin3_quantite").value;
+});
+let tarte_1=document.getElementById("tarte_1").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("tarte1_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("tarte1_price").innerHTML+=document.getElementById("tarte1_quantite").value;
+});
+let tarte_2=document.getElementById("tarte_2").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("tarte2_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("tarte2_price").innerHTML+=document.getElementById("tarte2_quantite").value;
+});
+let tarte_3=document.getElementById("tarte_3").addEventListener("click",() =>
+{   document.querySelector(".title").innerHTML="passer À la caisse";
+    document.getElementById("tarte3_cart").style.display="";
+    document.getElementById("finaliserbtn").style.display="";
+    document.getElementById("tarte3_price").innerHTML+=document.getElementById("tarte3_quantite").value;
+});
+
+document.getElementById("cake1_x").addEventListener("click",() => {
+document.getElementById("cake1_cart").style.display="none";
+cal_display_none();
+});
+document.getElementById("cake2_x").addEventListener("click",() => {
+    document.getElementById("cake2_cart").style.display="none";
+    cal_display_none();
+    });
+    document.getElementById("cake3_x").addEventListener("click",() => {
+        document.getElementById("cake3_cart").style.display="none";
+        cal_display_none();
+        });
+        document.getElementById("cake4_x").addEventListener("click",() => {
+            document.getElementById("cake4_cart").style.display="none";
+            cal_display_none();
+            });
+            document.getElementById("cake5_x").addEventListener("click",() => {
+                document.getElementById("cake5_cart").style.display="none";
+                cal_display_none();
+                });
+                document.getElementById("cake6_x").addEventListener("click",() => {
+                    document.getElementById("cake6_cart").style.display="none";
+                    cal_display_none();
+                    });
+
+document.getElementById("gateau1_x").addEventListener("click",() => {
+document.getElementById("gateau1_cart").style.display="none";
+cal_display_none();
+});
+document.getElementById("gateau2_x").addEventListener("click",() => {
+    document.getElementById("gateau2_cart").style.display="none";
+    cal_display_none();
+    });
+    document.getElementById("gateau3_x").addEventListener("click",() => {
+        document.getElementById("gateau3_cart").style.display="none";
+        cal_display_none();
+        });
+        document.getElementById("gateau4_x").addEventListener("click",() => {
+            document.getElementById("gateau4_cart").style.display="none";
+            cal_display_none();
+            });
+            document.getElementById("gateau5_x").addEventListener("click",() => {
+                document.getElementById("gateau5_cart").style.display="none";
+                cal_display_none();
+                });
+
+document.getElementById("vin1_x").addEventListener("click",() => {
+document.getElementById("vin1_cart").style.display="none";
+cal_display_none();
+});
+document.getElementById("vin2_x").addEventListener("click",() => {
+    document.getElementById("vin2_cart").style.display="none";
+    cal_display_none();
+    });
+    document.getElementById("vin3_x").addEventListener("click",() => {
+        document.getElementById("vin3_cart").style.display="none";
+        cal_display_none();
+        });
+
+document.getElementById("tarte1_x").addEventListener("click",() => {
+document.getElementById("tarte1_cart").style.display="none";
+cal_display_none();
+});
+document.getElementById("tarte2_x").addEventListener("click",() => {
+    document.getElementById("tarte2_cart").style.display="none";
+    cal_display_none();
+    });
+    document.getElementById("tarte3_x").addEventListener("click",() => {
+        document.getElementById("tarte3_cart").style.display="none";
+        cal_display_none();
+        });
+function cal_display_none()
+{       var cake_cart_none=0;
+        var gateau_cart_none=0;
+        var vin_cart_none=0;
+        var tarte_cart_none=0;
+        for(var i=0;i<cakes_name.length;i++)
+        if(document.getElementById("cake"+(i+1)+"_cart").style.display=="none")
+           cake_cart_none++;
+        for(var i=0;i<gateaux_name.length;i++)
+        if(document.getElementById("gateau"+(i+1)+"_cart").style.display=="none")
+        gateau_cart_none++;
+        for(var i=0;i<viennoiseries_name.length;i++)
+        if (document.getElementById("vin"+(i+1)+"_cart").style.display=="none")
+        vin_cart_none++;
+        for(var i=0;i<tartes_name.length;i++)
+        if(document.getElementById("tarte"+(i+1)+"_cart").style.display=="none")
+        tarte_cart_none++;
+
+        if(cake_cart_none==6 && gateau_cart_none==5 && vin_cart_none==3 && tarte_cart_none==3)
+{   
+    document.getElementById("finaliserbtn").style.display="none";
+    document.querySelector(".title").innerHTML="vous n'avez aucun produit dans le panier";
+}
+}
+
+// END PANIER
 //* fonction de recherche : 
 
 const chercher = () => {
